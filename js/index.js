5ecd4310aa9ed731d72d004cfb83f2984294fa07
joskeph <joskeph@gmail.com>
2023-05-24 06:56:27 +0530

    method checking

--- js/index.js
+++ js/index.js
@@ -1,23 +1,11 @@
-const oneSecond = () =>{
-    return new Promise((res,rej)=>{
-        setTimeout(() => {
-            res()
-        }, 1000);
-    })
+const a ={
+    method1 : () => " from method 1"
 }
 
-async function call1s(){
-    const result = await oneSecond();
-    console.log("statement 1", result)
-    
-    await oneSecond();
-    console.log("statement 2")
-    
-    await oneSecond();
-    console.log("statement 3")
-
-    await oneSecond();
-    console.log("statement 4")
+const b ={
+    method2:  () => " from method 2"
 }
 
-call1s()
+Object.setPrototypeOf(a,b)
+
+console.log(a.method2())
