07d7942afc422ac0a25472c7954453dc6af86559
Joseph Sony <126966283+joskeph@users.noreply.github.com>
2024-04-01 01:11:27 +0000

    cahnges

--- Joseph/all.js
+++ Joseph/all.js
@@ -1776,4 +1776,15 @@ function stopTimer(){
 }
 // ----------------------------------------
 
+//  Digital clock 
 
+function update(){
+    const now = new Date()
+    h=now.getHours().toString().padStart(2,'0')
+    m=now.getMinutes().toString().padStart(2,'0')
+    s=now.getSeconds().toString().padStart(2,'0')
+    document.getElementById("clock").textContent=h+":"+m+":"+s
+}
+
+update()
+setInterval(update,100)
